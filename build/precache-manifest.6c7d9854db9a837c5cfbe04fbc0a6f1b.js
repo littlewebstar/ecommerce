cb9f475ed5ffa93a78c3c9af895c059d0df17914
littlewebstar <littlewebstar@gmail.com>
2019-03-24 00:03:40 +0000

    Build Done

---
+++ build/precache-manifest.6c7d9854db9a837c5cfbe04fbc0a6f1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dcf32d7beac6a296fc62",
+    "url": "/static/js/main.2af3e135.chunk.js"
+  },
+  {
+    "revision": "38e9280959dd5f0d5c78",
+    "url": "/static/js/2.88ea1fd6.chunk.js"
+  },
+  {
+    "revision": "dcf32d7beac6a296fc62",
+    "url": "/static/css/main.08df79fb.chunk.css"
+  },
+  {
+    "revision": "b880b3239a33f342e48b5f0fa9217ba7",
+    "url": "/index.html"
+  }
+];
